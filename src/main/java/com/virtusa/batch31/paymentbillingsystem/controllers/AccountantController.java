1a9dbcdbce25a1034f0b0a771969bf248e995b73
ritik2020 <ritik20023@gmail.com>
2021-10-06 23:06:54 +0530

    deleted controllers/student and dao package

--- src/main/java/com/virtusa/batch31/paymentbillingsystem/controllers/AccountantController.java
+++ src/main/java/com/virtusa/batch31/paymentbillingsystem/controllers/AccountantController.java
@@ -1,9 +1,11 @@
 package com.virtusa.batch31.paymentbillingsystem.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/accountant")
+@RestController
+@RequestMapping("/accountant")
 public class AccountantController {
 	
 	@GetMapping("/")
