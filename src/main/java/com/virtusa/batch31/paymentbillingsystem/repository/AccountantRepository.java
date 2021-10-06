ae2f39796342692ae8248a1d8ced8291e983911f
ritik2020 <ritik20023@gmail.com>
2021-10-06 13:45:21 +0530

    added repositories

---
+++ src/main/java/com/virtusa/batch31/paymentbillingsystem/repository/AccountantRepository.java
@@ -0,0 +1,9 @@
+package com.virtusa.batch31.paymentbillingsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.virtusa.batch31.paymentbillingsystem.entities.Accountant;
+
+public interface AccountantRepository extends JpaRepository<Accountant, Integer> {
+
+}
