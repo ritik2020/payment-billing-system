591fb5972e3134df04b07a8ee1e086abb826f654
nirmet <nirmetkaushik@gmail.com>
2021-10-07 14:54:31 +0530

    created admin and payment controller

--- src/main/java/com/virtusa/batch31/paymentbillingsystem/controllers/adminController.java
+++ src/main/java/com/virtusa/batch31/paymentbillingsystem/controllers/adminController.java
@@ -1,48 +1,42 @@
-package com.virtusa.batch31.paymentbillingsystem.controllers;
+	package com.virtusa.batch31.paymentbillingsystem.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+	import java.util.List;
 
-import com.virtusa.batch31.paymentbillingsystem.entities.Admin;
-import com.virtusa.batch31.paymentbillingsystem.services.AdminService;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PathVariable;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.PutMapping;
+	import org.springframework.web.bind.annotation.RequestBody;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+	import com.virtusa.batch31.paymentbillingsystem.entities.Admin;
+	import com.virtusa.batch31.paymentbillingsystem.services.AdminService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-@RestController
-
-public class adminController {
-	
-	@Autowired
-	private AdminService service;
-	@PostMapping("/createAdmin")
-	public Admin createAdmin(@RequestBody Admin admin) {
-		return service.createAdmin(admin);
+	@RestController
+	@RequestMapping("/admin")
+	public class AdminController {
+		
+		@Autowired
+		private AdminService service;
+		@PostMapping("/createAdmin")
+		public Admin createAdmin(@RequestBody Admin admin) {
+			return service.createAdmin(admin);
+		}
+	@GetMapping("/getAdmin")	
+		public Admin getAdmin(@PathVariable int id) {
+			return service.getAdmin(id);
+		}
+	@GetMapping("/getAllAdmins")	
+		public List<Admin> getAllAdmins(@PathVariable int id){
+			return service.getAllAdmins(id);
 	}
-@GetMapping("/getAdmin")	
-	public Admin getAdmin(@PathVariable int id) {
-		return service.getAdmin(id);
+
+	@PutMapping("/update")
+	public Admin UpdateAdmin(@RequestBody Admin admin) {
+		return service.updateAdmin(admin);
+		
 	}
-@GetMapping("/getAllAdmins")	
-	public List<Admin> getAllAdmins(@PathVariable int id){
-		return service.getAllAdmins(id);
 }
 
-@PutMapping("/update")
-public Admin UpdateAdmin(@RequestBody Admin admin) {
-	return service.updateAdmin(admin);
-	
-}
-
-
-	
-	
-	
-
-}
