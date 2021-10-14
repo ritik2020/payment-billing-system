1eb30b42c993b4758eec852b593df5792d4ca46d
ritik2020 <ritik20023@gmail.com>
2021-10-14 09:47:07 +0530

    added try catch in interceptor

--- src/main/java/com/virtusa/batch31/paymentbillingsystem/RequestInterceptor.java
+++ src/main/java/com/virtusa/batch31/paymentbillingsystem/RequestInterceptor.java
@@ -15,47 +15,54 @@ import io.jsonwebtoken.Claims;
 
 @Component
 public class RequestInterceptor implements HandlerInterceptor {
-	
+
 	@Autowired
 	private JwtUtilService jwtUtilService;
-	
+
 	@Autowired
 	private ApiRoleMapping apiRoleMapping;
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		String token = request.getHeader("token");
-		String method = request.getMethod();
-		
-		if(method.equals("OPTIONS")){
-			return true;
-		}
+		try {
+			String token = request.getHeader("token");
+			String method = request.getMethod();
 
-		if(token==null || !jwtUtilService.validateToken(token)) {
+			if (method.equals("OPTIONS")) {
+				return true;
+			}
+
+			if (token == null || !jwtUtilService.validateToken(token)) {
+				response.getWriter().write("Unauthorized Request");
+				response.setStatus(401);
+				return false;
+			}
+			String uri = request.getRequestURI().toString();
+			Claims claims = jwtUtilService.extractAllClaims(token);
+			String role = (String) claims.get("role");
+			System.out.println(uri);
+			System.out.println(role);
+			List<String> allowedRoles = apiRoleMapping.getAllowedRoles(uri, method);
+			System.out.println(allowedRoles);
+			if (allowedRoles == null) {
+				response.getWriter().write("You are not authorized to access this resource");
+				response.setStatus(401);
+				return false;
+			}
+			if (allowedRoles.contains(role)) {
+				return true;
+			} else {
+				response.getWriter().write("You are not authorized to access this resource");
+				response.setStatus(401);
+				return false;
+			}
+
+		} catch (Exception e) {
 			response.getWriter().write("Unauthorized Request");
 			response.setStatus(401);
 			return false;
 		}
-		String uri = request.getRequestURI().toString();
-		Claims claims = jwtUtilService.extractAllClaims(token);
-		String role = (String)claims.get("role");
-		System.out.println(uri);
-		System.out.println(role);
-		List<String> allowedRoles = apiRoleMapping.getAllowedRoles(uri, method);
-		System.out.println(allowedRoles);
-		if(allowedRoles==null){
-			response.getWriter().write("You are not authorized to access this resource");
-			response.setStatus(401);
-			return false;
-		}
-		if(allowedRoles.contains(role)) {
-			return true;
-		}
-		else {
-			response.getWriter().write("You are not authorized to access this resource");
-			response.setStatus(401);
-			return false;
-		}
-	}	
+
+	}
 }
