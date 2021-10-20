39a572a57201c7b1cbaf9e0a383230f7098a337f
ritik2020 <ritik20023@gmail.com>
2021-10-20 12:36:08 +0530

    hide my smtp email and password for security

--- src/main/java/com/virtusa/batch31/paymentbillingsystem/services/EmailSenderService.java
+++ src/main/java/com/virtusa/batch31/paymentbillingsystem/services/EmailSenderService.java
@@ -6,6 +6,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -18,9 +19,12 @@ public class EmailSenderService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendSimpleEmail(String toEmail, String body, String subject){
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("ritik20023@gmail.com");
+        message.setFrom(fromEmail);
         message.setTo(toEmail);
         message.setText(body);
         message.setSubject(subject);
@@ -34,7 +38,7 @@ public class EmailSenderService {
 
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
 
-        mimeMessageHelper.setFrom("ritik20023@gmail.com");
+        mimeMessageHelper.setFrom(fromEmail);
         mimeMessageHelper.setTo(toEmail);
         mimeMessageHelper.setText(body);
         mimeMessageHelper.setSubject(subject);
